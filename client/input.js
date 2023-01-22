366bc0e25de5587ed5dfe3a9581cd379fcd759eb
nicholasfong <nfong01@students.cs.ubc.ca>
2023-01-22 02:05:25 -0800

    server client connection

---
+++ client/input.js
@@ -0,0 +1,23 @@
+// Generate recipies
+function changeString(str) {
+    return str.replace(/ /g, "+");
+}
+
+function generaterecipies() {
+
+ingredient = document.getElementById("ingredients").value;
+ingredient = changeString(ingredient);
+console.log(ingredient)
+
+fetch('http://localhost:3001/getrecipedata', 
+{method: "POST", body : JSON.stringify({ingredient}), headers:{'content-type': 'application/json'}})
+.then(response => response.json()).then ((finalrecipe) => {
+  console.log(finalrecipe);
+
+  let finalrecipeOB = finalrecipe;
+  console.log(finalrecipeOB);
+
+})
+
+
+}
