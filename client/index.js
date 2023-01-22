3eecf2a88e5a79f7f56872b1de4dd8a4d009b723
Matthew <matthewyungisworking@gmail.com>
2023-01-21 17:51:41 -0800

    added scroll effects

---
+++ client/index.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+        entry.target.classList.toggle('show', entry.isIntersecting)
+    }
+    else {entry.target.classList.remove('show', entry.isIntersecting)}
+});
+});
+
+
+
+const hiddenElement = document.querySelectorAll('.hidden')
+hiddenElement.forEach((el) => observer.observe(el));
+
